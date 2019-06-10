e94d07b3ab7e2373cc88633489ea3bc50f5d5a9e
sn1ch <sn1ch@list.ru>
2019-06-10 23:13:43 +0300

    davai

---
+++ hm_decorators/main.py
@@ -0,0 +1,26 @@
+import random
+from datetime import datetime
+from pprint import pprint
+
+
+def log(old_function):
+    def new_function(*args, **kwargs):
+        result = old_function(*args, **kwargs)
+        with open('data/log.txt', 'a', encoding='utf8')as f:
+            time_start = datetime.isoformat(datetime.now(), sep=' ')
+            f.write(
+                f"{time_start} function name: {new_function.__name__} attributes: {args}{kwargs} result: {result}\n")
+        return result
+    return new_function
+
+
+@log
+def random_list(size):
+    random_list = [random.random() for _ in range(size)]
+    return random_list
+
+
+if __name__ == '__main__':
+    random_list(100)
+
+
---
+++ hm_decorators/main2.py
@@ -0,0 +1,26 @@
+import random
+from datetime import datetime
+from pprint import pprint
+
+
+def set_path_log(path):
+    def log(old_function):
+        def new_function(*args, **kwargs):
+            result = old_function(*args, **kwargs)
+            with open(path, 'a', encoding='utf8')as f:
+                time_start = datetime.isoformat(datetime.now(), sep=' ')
+                f.write(
+                    f"{time_start} function name: {new_function.__name__} attributes: {args}{kwargs} result: {result}\n")
+            return result
+        return new_function
+    return log
+
+
+@set_path_log('data/logs2.txt')
+def random_list(size):
+    random_list = [random.random() for _ in range(size)]
+    return random_list
+
+
+if __name__ == '__main__':
+    random_list(100)
